5cf93ffd6526cf006cfb312ea94b7c111b396ff5
KamoD-a <70842126+KamoD-a@users.noreply.github.com>
2024-10-24 13:54:59 +0200

    Added md5 bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <NavBar/>
+  <SlideShow/>
+  
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import SlideShow from './components/SlideShow.vue';
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    SlideShow
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
